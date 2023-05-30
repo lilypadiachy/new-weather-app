4841cc367f45c4bfa1e68b17e53aaed99b1ad468
Lilian Padiachy <lilyypad91@gmail.com>
2023-05-30 20:19:52 +0200

    Built date, search and current functionality in js
    Date: Format to day and time
    Search: Search City name entered
    Current: Use GPS coords to find weather in current location

--- script.js
+++ script.js
@@ -1,1 +1,65 @@
-alert("Hello World")
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let day = days[now.getDay()];
+
+function formatDate() {
+  let currentDateElement = document.getElementById("current-date");
+  currentDateElement.innerHTML = `${day}, ${hours}:${minutes}`;
+}
+
+formatDate();
+
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-text-input");
+  let h3 = document.querySelector("#city-name");
+  let temperatureElement = document.querySelector("#temperature");
+
+  if (searchInput.value) {
+    let apiKey = "b3a4489ba69b29805e86d4aac8b269f2";
+    let city = searchInput.value;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+    axios.get(apiUrl).then(function (response) {
+      let temperature = Math.round(response.data.main.temp);
+      h3.innerHTML = city;
+      temperatureElement.innerHTML = temperature;
+    });
+  }
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
+
+function showWeather(response) {
+  let h3 = document.querySelector("h3");
+  let temperature = Math.round(response.data.main.temp);
+  let temperatureElement = document.getElementById("temperature");
+  h3.innerHTML = `${response.data.name}`;
+  temperatureElement.innerHTML = `${temperature}`;
+}
+
+function getCurrentPosition(position) {
+  console.log(position);
+  let apiKey = "34ae1065362d42545661451bda2b8a1f";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+  axios.get(url).then(showWeather);
+}
+
+let currentLocationButton = document.getElementById("current-location");
+currentLocationButton.addEventListener("click", function () {
+  navigator.geolocation.getCurrentPosition(getCurrentPosition);
+});
